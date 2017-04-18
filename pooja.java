a131eb589fc6f418c1e43f56880438c5d265bf24
pooja-ashokan <ashokanpooja@gmail.com>
2017-04-18 19:19:50 +0530

    pooja

---
+++ pooja.java
@@ -0,0 +1,18 @@
+import java.io.*;
+class Const Overloading
+{
+public int roll Num;
+Constoverloading()
+{
+roll Num=100;
+System.out.println("no parameters"+roll Num);
+}
+Const Overloading (int rnum)
+{
+roll Num=roll Num+rnum;
+System.out.println("with parameter"+roll Num);
+}
+public static void main(String args[])
+{
+Const Overloading obj=new Constoverloading();
+Constoverloading obj1=new ConstOverloading (200);
